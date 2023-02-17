6b54c94cec5bf2eb7c53fd386fdaffeb4553cfa8
Michał Praszmo <nazywam@gmail.com>
2023-02-17 18:20:28 +0100

    Bump karton-core version to 5 (#13)

--- karton/misp_pusher/misp_pusher.py
+++ karton/misp_pusher/misp_pusher.py
@@ -1,9 +1,20 @@
+import argparse
 from uuid import UUID, uuid5
 
 from karton.core import Config, Karton, Task
 from mwdb_iocextract import parse  # type: ignore
 from mwdblib.util import config_dhash  # type: ignore
 from pymisp import ExpandedPyMISP, MISPEvent
+
+
+def http_url(value: str) -> str:
+    """Ensure that provided value looks like a HTTP URL (https://sth),
+    and strip a trailing slash. The goal is to avoid confusion between
+    "url.com", "http://url.com", "http://url.com/", etc.
+    """
+    if not value.startswith("http"):
+        raise ValueError("URL should start with http[s]://")
+    return value.rstrip("/")
 
 
 class MispPusher(Karton):
@@ -19,7 +30,16 @@ class MispPusher(Karton):
     # Event UUIDs are generated deterministically from a (root, cfg_hash) pair.
     CONFIG_NAMESPACE = UUID("dc232ceb-a523-41a1-98f4-8d3d52ec6eff")
 
-    def process(self, task: Task) -> None:  # type: ignore
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+
+        if not self.config.get("misp", "url"):
+            raise RuntimeError("Misp config section is missing the url parameter")
+
+        if not self.config.get("misp", "key"):
+            raise RuntimeError("Misp config section is missing the key parameter")
+
+    def process(self, task: Task) -> None:
         config = task.get_payload("config")
         family = task.headers["family"]
         dhash = config_dhash(config)
@@ -37,72 +57,36 @@ class MispPusher(Karton):
         event.add_tag(f"mwdb:family:{family}")
         event.info = f"Malware configuration ({family})"
 
-        if self.mwdb_url is not None:
-            event.add_attribute("link", f"{self.mwdb_url}/config/{dhash}")
+        mwdb_url = self.config.get("misp", "mwdb_url")
+
+        if mwdb_url is not None:
+            event.add_attribute("link", f"{mwdb_url}/config/{dhash}")
 
         for o in iocs.to_misp():
             event.add_object(o)
 
-        event.published = self.misp_published
+        event.published = self.config.getboolean("misp", "published", False)
 
-        misp = ExpandedPyMISP(self.misp_url, self.misp_key, self.misp_verifycert)
+        misp = ExpandedPyMISP(
+            http_url(self.config.get("misp", "url")),
+            self.config.get("misp", "key"),
+            not self.config.getboolean("misp", "insecure", False),
+        )
         misp.add_event(event)
 
-    def __init__(
-        self,
-        config: Config,
-        misp_url: str,
-        misp_key: str,
-        misp_published: bool = False,
-        misp_verifycert: bool = True,
-        mwdb_url: str = None,
-    ) -> None:
-        """
-        Create instance of the MispPusher.
-
-        :param config: Karton configuration object
-        :param misp_url: URL of the paired MISP instance
-        :param misp_key: API key of the paired MISP instance
-        :param misp_published: Publish MISP Events Boolean
-        :param misp_verifycert: "False" to skip TLS cert validation (unrecommended)
-        :param mwdb_url: Optional mwdb url, for `link` MISP attribute
-        """
-
-        super().__init__(config)
-
-        self.misp_url = misp_url
-        self.misp_key = misp_key
-        self.misp_published = misp_published
-        self.misp_verifycert = misp_verifycert
-        self.mwdb_url = mwdb_url
-
     @classmethod
-    def args_parser(cls):
-        def http_url(value):
-            """Ensure that provided value looks like a HTTP URL (https://sth),
-            and strip a trailing slash. The goal is to avoid confusion between
-            "url.com", "http://url.com", "http://url.com/", etc.
-            """
-            if not value.startswith("http"):
-                raise ValueError("URL should start with http[s]://")
-            return value.rstrip("/")
-
+    def args_parser(cls) -> argparse.ArgumentParser:
         parser = super().args_parser()
         parser.add_argument(
             "--misp-url",
-            type=http_url,
-            required=True,
             help="URL of the paired MISP instance",
         )
         parser.add_argument(
             "--misp-key",
-            required=True,
             help="API key of the paired MISP instance",
         )
         parser.add_argument(
             "--misp-published",
-            required=False,
-            default=False,
             action="store_true",
             help="Publish MISP Events",
         )
@@ -119,17 +103,16 @@ class MispPusher(Karton):
         return parser
 
     @classmethod
-    def main(cls):
-        parser = cls.args_parser()
-        args = parser.parse_args()
-
-        config = Config(args.config_file)
-        service = cls(
-            config,
-            args.misp_url,
-            args.misp_key,
-            args.misp_published,
-            not args.misp_insecure,
-            args.mwdb_url,
+    def config_from_args(cls, config: Config, args: argparse.Namespace) -> None:
+        super().config_from_args(config, args)
+        config.load_from_dict(
+            {
+                "misp": {
+                    "url": args.misp_url,
+                    "key": args.misp_key,
+                    "published": args.misp_published,
+                    "insecure": args.misp_insecure,
+                    "mwdb_url": args.mwdb_url,
+                }
+            }
         )
-        service.loop()
